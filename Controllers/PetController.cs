08b1b844fec0a635332811348580ee2151209c96
= <alexandressoares383@gmail.com>
2025-09-07 20:16:44 -0300

    feat: endpoint pra recuperar lista

--- Controllers/PetController.cs
+++ Controllers/PetController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.Register;
 using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
@@ -13,6 +14,7 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestPetJson request)
     {
         var response = new RegisterPetUseCase().Execute(request);
@@ -22,11 +24,28 @@ public class PetController : ControllerBase
     [HttpPut]
     [Route("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
     public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
     {
         var useCase = new UpdatePetUseCase();
         useCase.Execute(id, request);
         
         return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllPetJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetAll()
+    {
+        var useCase = new GetAllPetsUseCase();
+        var response = useCase.Execute();
+
+        if (response.Pets.Any())
+        {
+            return Ok(response);
+        }
+
+        return Ok();
     }
 }
---
+++ Petfolio.Application/UseCases/Pets/GetAll/GetAllPetsUseCase.cs
@@ -0,0 +1,22 @@
+using Petfolio.Communication.Response;
+
+namespace Petfolio.Application.UseCases.Pets.GetAll;
+
+public class GetAllPetsUseCase
+{
+    public ResponseAllPetJson Execute()
+    {
+        return new ResponseAllPetJson
+        {
+            Pets = new List<ResponseShortPetJson>
+            {
+                new ResponseShortPetJson
+                {
+                    Id = 1,
+                    Name = "Charlie",
+                    Type = Communication.Enums.PetType.Dog
+                }
+            }
+        };
+    }
+}
